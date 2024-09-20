ce5a26d68516a8ab2809f9658e21af03ca1bb32c
Van Dao <dao28901@gmail.com>
2024-09-20 13:51:15 +0700

    hehe

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -70,12 +70,31 @@ void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
+  final corsHeader = createMiddleware(requestHandler: (req) {
+    if (req.method == 'OPTIONS') {
+      return Response.ok("", headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, HEAD',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      });
+    }
+    return null;
+  }, responseHandler: (res) {
+    return res.change(headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, HEAD',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    });
+  });
+
   // Configure a pipeline that logs requests.
-  final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+  final handler = Pipeline()
+      .addMiddleware(corsHeader)
+      .addMiddleware(logRequests())
+      .addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
+  print('Server đang chạy tại http://${server.address.host}:${server.port}');
 }
--- fontend/lib/main.dart
+++ fontend/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +12,81 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: "siuuuuuuuuuu",
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: MyHomePage(),
         ),
       ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controller = TextEditingController();
+  String responseMessage = '';
+  Future<void> sendName() async {
+    final name = controller.text;
+    controller.clear();
+    final url = Uri.parse('http://localhost:8080/api/v1/submit');
+    try {
+      final response = await http
+          .post(
+            url,
+            headers: {'Content-Type': 'application/json'},
+            body: json.encode({'name': name}),
+          )
+          .timeout(const Duration(seconds: 10));
+      // Kiểm tra nếu phản hồi có nội dung
+      if (response.body.isNotEmpty) {
+        // Giải mã phản hồi từ server
+        final data = json.decode(response.body);
+
+        // Cập nhật trạng thái với thông điệp nhận được từ server
+        setState(() {
+          responseMessage = data['message'];
+        });
+      } else {
+        // Phản hồi không có nội dung
+        setState(() {
+          responseMessage = 'Không nhận được phản hồi từ server';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        responseMessage = 'Đã xảy ra lỗi: ${e.toString()}';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SIUUUUUUUUUUUUUU"),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            children: [
+              TextField(
+                controller: controller,
+                decoration: const InputDecoration(labelText: "Ten"),
+              ),
+              SizedBox(height: 20),
+              FilledButton(onPressed: sendName, child: const Text("send")),
+              Text(
+                responseMessage,
+                style: Theme.of(context).textTheme.titleLarge,
+              )
+            ],
+          )),
     );
   }
 }
